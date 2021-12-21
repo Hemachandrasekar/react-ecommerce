2cd30014bc7866dd27bd337fe51b4b1dbf3b8eb5
Hema Chandrasekar <m.hema10@gmail.com>
2021-12-21 15:54:31 +0530

    Hoem page completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, Instagram, Twitter, Pinterest } from '@material-ui/icons';
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  Pinterest,
+  Room,
+  Phone,
+  Mail,
+} from '@material-ui/icons';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -6,17 +14,63 @@ const Container = styled.div`
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
-const Logo = styled.div``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 200px;
+`;
+
 const Footer = () => {
   return (
     <Container>
@@ -24,25 +78,56 @@ const Footer = () => {
         <Logo>LAMA.</Logo>
         <Desc>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
         </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="385999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Usefull Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Women Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Whishlist</ListItem>
+          <ListItem>Whishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact </Title>
+        <ContactItem>
+          <Room style={{ marginRight: '10px' }} />
+          622 Dixie Path, South Americe Towwer
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: '10px' }} />
+          +91 098764567
+        </ContactItem>
+        <ContactItem>
+          <Mail style={{ marginRight: '10px' }} />
+          lamaecart@gmail.com
+        </ContactItem>
+        <Payment src="../images/payment.png" />
+      </Right>
     </Container>
   );
 };
