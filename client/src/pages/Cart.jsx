579242d9e10c1974b6626e2dbd63d6ffbe405bb7
Hema Chandrasekar <m.hema10@gmail.com>
2021-12-23 19:26:38 +0530

    Cart page completed

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
@@ -19,12 +20,17 @@ const Top = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 20px;
 `;
 
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
+  border: ${(props) => props.type === 'filled' && 'none'};
+  background-color: ${(props) =>
+    props.type === 'filled' ? 'black' : 'transparent'};
+  color: ${(props) => props.type === 'filled' && 'white'};
 `;
 const TopTexts = styled.div``;
 
@@ -41,11 +47,98 @@ const Bottom = styled.div`
 const Info = styled.div`
   flex: 3;
 `;
-const Summary = styled.div`
-  flex: 1;
-  background-color: red;
+const Product = styled.div`
+  display: flex;
+  justify: space-between;
 `;
 
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === 'total' && '500'};
+  font-size: ${(props) => props.type === 'total' && '24px'};
+`;
+const SummaryItemTitle = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #333;
+  font-weight: 600;
+  color: #fff;
+`;
 const Cart = () => {
   return (
     <Container>
@@ -59,11 +152,84 @@ const Cart = () => {
             <TopText>Shopping Bag(2)</TopText>
             <TopText>Your Whishlist (0)</TopText>
           </TopTexts>
-          <TopButton>CHECKOUT NOW</TopButton>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
         <Bottom>
-          <Info>gbhnj</Info>
-          <Summary>Sumay</Summary>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="./images/product/shoes.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product :</b> JESSIE TOURSER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID :</b>9876543212
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size :</b>37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                  <ProductPrice>$ 30</ProductPrice>
+                </ProductAmountContainer>
+              </PriceDetail>
+            </Product>
+
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="./images/product/2.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product :</b> DINEM T-SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID :</b>9876543212
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size :</b>M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                  <ProductPrice>$ 20</ProductPrice>
+                </ProductAmountContainer>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemTitle>Sub Total</SummaryItemTitle>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+
+            <SummaryItem>
+              <SummaryItemTitle>Estimate Shipping</SummaryItemTitle>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemTitle>Shipping Discount</SummaryItemTitle>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemTitle> Total</SummaryItemTitle>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
